bb0484a808f13e280a1765422e107917ab049896
hackdante <genoma3d@gmail.com>
2022-08-20 09:52:18 -0500

    First version 0.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { CounterComponent } from "./components/TEST/counter/CounterComponent";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { Navbar } from "./components/globals/navbar/Navbar";
+import { Footer } from "./components/globals/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="test/counter" element={<CounterComponent />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/globals/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>Territorio Virtual v.0.0.0</div>
+  )
+}
